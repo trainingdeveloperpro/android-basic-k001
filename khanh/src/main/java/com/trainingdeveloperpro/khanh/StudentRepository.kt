d5bac7cf620f12a0ffc57e97d6f22af11ec35f87
Nguyen Quoc Khanh <nguyenkhanhcv@gmail.com>
2019-08-17 21:52:28 +0700

    Add main files

---
+++ khanh/src/main/java/com/trainingdeveloperpro/khanh/StudentRepository.kt
@@ -0,0 +1,17 @@
+package com.trainingdeveloperpro.khanh
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+
+class StudentRepository(private val studentDao: StudentDao) {
+    val allStudent: LiveData<List<Student>> = studentDao.getAllStudent()
+    @WorkerThread
+    suspend fun insert(student: Student){
+        studentDao.insert(student)
+    }
+
+    @WorkerThread
+    fun delete(student: Student){
+        studentDao.deleteStudent(student)
+    }
+}
